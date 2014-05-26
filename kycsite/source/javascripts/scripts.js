3fe9216c33e7bdff05a1157dd78dea8a33cf1b40
gingerfoxx <sarahcadwell@gmail.com>
2014-05-26 16:24:19 -0700

    OMG INSTAGRAM WORKS OMGOMGOMG

--- kycsite/source/javascripts/scripts.js
+++ kycsite/source/javascripts/scripts.js
@@ -5,10 +5,9 @@ $(function() {
         
        var feed = new Instafeed({
         get: 'tagged',
-        tagName: 'polaroid',
+        tagName: 'hipster',
         clientId: 'bc3dfcbf9a424581909fff9682de9215',
-        template: '<a class="animation" href="{{link}}"><img src="{{image}}" /></a>',
-        sortBy: 'random'
+
     });
     feed.run();
 
