d31dab2aac5141e2e199da30414a744553da6550
gingerfoxx <sarahcadwell@gmail.com>
2014-05-26 13:52:47 -0700

    More Instagram….

--- kycsite/source/js/scripts.js
+++ kycsite/source/js/scripts.js
@@ -2,16 +2,9 @@
 $(function()
 
 {
-  $('.instagram').on('willLoadInstagram', function(event, options) {
-    console.log(options);
-  });
-  $('.instagram').on('didLoadInstagram', function(event, response) {
-    console.log(response);
-  });
-  $('.instagram').instagram({
-    hash: 'hipster',
-    clientId: 'bc3dfcbf9a424581909fff9682de9215'
-  });
+$('.instagram').instagram({
+  hash: 'love',
+  clientId: 'bc3dfcbf9a424581909fff9682de9215'
 });
 
 });
