a27846b8d2b1ade25216ebfaf3f79f8fe223b302
gingerfoxx <sarahcadwell@gmail.com>
2014-05-26 16:38:11 -0700

    Instagram is pretty!

--- kycsite/source/javascripts/scripts.js
+++ kycsite/source/javascripts/scripts.js
@@ -5,8 +5,9 @@ $(function() {
         
        var feed = new Instafeed({
         get: 'tagged',
-        tagName: 'hipster',
+        tagName: 'portland',
         clientId: 'bc3dfcbf9a424581909fff9682de9215',
+        limit: 6,
 
     });
     feed.run();
