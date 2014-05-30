be8b55880727f5795ee22c45d00291a356282fa3
gingerfoxx <sarahcadwell@gmail.com>
2014-05-30 12:43:59 -0700

    Scroll To working more
    Lines up with whole div

--- kycsite/source/javascripts/scripts.js
+++ kycsite/source/javascripts/scripts.js
@@ -17,17 +17,17 @@ $(function() {
     
     
     $('.visituslink').click(function() {
-		$(window).scrollTo('#visitus', 1000);
-		/* $('visitus').addClass('.a:hover'); */	
+		$(window).scrollTo('#visitus', 1000, {offset: -75});
+		
 	});
 	
     $('.schedulelink').click(function() {
-		$(window).scrollTo('#schedule', 1000);
+		$(window).scrollTo('#schedule', 1000, {offset: -75});
 		/* $('visitus').addClass('.a:hover'); */	
 	});
 	
 	    $('.tourslink').click(function() {
-		$(window).scrollTo('#tours', 1000);
+		$(window).scrollTo('#tours', 1000, {offset: -74});
 		/* $('visitus').addClass('.a:hover'); */	
 	});
     
